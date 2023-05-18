aa70c87baafe2a487dcf1670c5c255112639238d
mzarchi <me@zartweety.ir>
2023-05-18 23:33:47 +0330

    Rename login class to user class

--- codes/user.py
+++ codes/user.py
@@ -1,10 +1,8 @@
 import sys
-
 import telethon
+
 from telethon.sync import TelegramClient
-
 from .utils import ValidatePhone, LOGIN_MESSAGE
-
 
 
 class UserManager:
@@ -13,10 +11,12 @@ class UserManager:
 
     __api_hash = None
     __api_id = None
+    __session = None
 
-    def __init__(self, api_id, api_hash):
+    def __init__(self, api_id, api_hash, session):
         self.__api_hash = api_hash
         self.__api_id = api_id
+        self.__session = session
 
     def login(self):
         """
@@ -25,11 +25,10 @@ class UserManager:
         print(LOGIN_MESSAGE)
         PhoneNumber = input("\nEnter a Phone number: ")
         if not ValidatePhone(PhoneNumber):
-            sys.exit("[x] Phone number is not valid, please enter a valid phone number ...")
+            sys.exit(
+                "[x] Phone number is not valid, please enter a valid phone number ...")
 
-        name = PhoneNumber
-        client = TelegramClient(
-            f'{name.replace("+", "")}', self.__api_id, self.__api_hash)
+        client = TelegramClient(self.__session, self.__api_id, self.__api_hash)
         client.connect()
         client.send_code_request(PhoneNumber, force_sms=False)
         value = input("Enter Login Code: ")
@@ -39,3 +38,13 @@ class UserManager:
             password = input("Enter Your 2-Step Code: ")
             client.sign_in(password=password)
         print("Successfully Connect!\nSession created at sessions dir")
+
+    def sendMessage(self, username: str, message: str):
+
+        client = TelegramClient(self.__session, self.__api_id, self.__api_hash)
+        client.start()
+
+        client.send_message(
+            entity=client.get_entity(username),
+            message=message
+        )
