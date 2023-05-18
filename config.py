60bccc465163f11bc8bc15c88080f820d2e39aab
mzarchi <me@zartweety.ir>
2023-05-18 23:34:29 +0330

    Fixed bugs

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+# Using dotenv to use environment variables
+from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables
+env = os.environ
+
 # read api id and hash from envs
-api_id = os.getenv("API_ID", None)
-api_hash = os.getenv("API_HASH", None)
+api_id = env["API_ID"]
+api_hash = env["API_HASH"]
